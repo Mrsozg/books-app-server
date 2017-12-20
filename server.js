f2c68fbe9d8e9db460870964cdc634e01d6b8166
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-19 21:42:02 -0800

    server.js is modified

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-
-'use strict';
-
+'use strict'
 
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+
 const app = express();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
-const client = new pg.Client(process.env.DATABASE_URL);
+
+const client = new pg.Client(DATABASE_URL);
 client.connect();
-
-
-client.on('error', err => {console.error(err);});
+client.on('error', console.error);
 
 app.use(cors());
-app.get('/',(req,res)=>res.send('Testing 1,2,3'));
-app.get('*',(req,res)=>res.redirect(CLIENT_URL));
-app.listen(PORT,()=> console.log(`listening on port:${PORT}`));
+
+app.get('/api/v1/books', (req, res) => {
+  client.query('SELECT book_id, title, author, image_url FROM books;')
+  .then(results => res.send(results.rows))
+  .catch(console.error)
+});
+
+app.all('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
