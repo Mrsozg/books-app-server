c3bd38bcf1932dcecdd5f9e0393f0f019241ed15
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-19 20:52:45 -0800

    files are reorganized

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
+'use strict'
 
-'use strict';
+// Application Dependencies
+const express = require('express')
+const cors = require('cors')
+const pg = require('pg')
 
+// App Setup
+const app = express()
+const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+const CLIENT_URL = process.env.CLIENT_URL
 
-const express = require('express');
-const cors = require('cors');
-const pg = require('pg');
-const app = express();
-const PORT = process.env.PORT;
-const CLIENT_URL = process.env.CLIENT_URL;
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
+// DATABASE Setup
+const client = new pg.Client(DATABASE_URL)
+client.connect()
+client.on('error', console.error)
 
+app.use(cors())
 
-client.on('error', err => {console.error(err);});
-
-app.use(cors());
-
-app.get('/api/v1/books',(req,res) => {
-  client.query('SELECT book_id, title, author, image_url FROM BOOKS;')
+app.get('/api/v1/books', (req, res) => {
+  client.query('SELECT book_id, title, author, image_url FROM books;')
   .then(results => res.send(results.rows))
   .catch(console.error)
-});
-app.all('*',(req,res)=>res.redirect(CLIENT_URL));
-app.listen(PORT,()=> console.log(`listening on port:${PORT}`));
+})
+
+app.all('*', (req, res) => res.redirect(CLIENT_URL))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
