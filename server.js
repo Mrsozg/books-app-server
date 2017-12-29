7e615f118a628154d0543d9ea72482bd994a9adc
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-28 21:53:13 -0800

    fixed find method

--- server.js
+++ server.js
@@ -28,6 +28,52 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('/'));
 
+app.get('/api/v1/books/find', (req, res) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  let query = ''
+  if(req.query.title) query += `+intitle:${req.query.title}`;
+  if(req.query.author) query += `+inauthor:${req.query.author}`;
+  if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
+
+  superagent.get(url)
+    .query({'q': query})
+    .query({'key': GOOGLE_API_KEY})
+    .then(response => response.body.items.map((book, idx) => {
+      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
+      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+      return {
+        title: title ? title : 'No title available',
+        author: authors ? authors[0] : 'No authors available',
+        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
+        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
+        description: description ? description : 'No description available',
+        book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
+      }
+    }))
+    .then(arr => res.send(arr))
+    .catch(console.error)
+});
+
+app.get('/api/v1/books/find/:isbn', (req, res) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  superagent.get(url)
+    .query({ 'q': `+isbn:${req.params.isbn}`})
+    .query({ 'key': GOOGLE_API_KEY })
+    .then(response => response.body.items.map((book, idx) => {
+      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
+      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+
+      return {
+        title: title ? title : 'No title available',
+        author: authors ? authors[0] : 'No authors available',
+        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
+        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
+        description: description ? description : 'No description available',
+      }
+    }))
+    .then(book => res.send(book[0]))
+    .catch(console.error)
+});
 
 app.get('/api/v1/books', (req, res) => {
   client.query('SELECT book_id, title, author, image_url FROM books;')
@@ -63,55 +109,6 @@ app.put('/api/v1/books/:id', (req, res) => {
   .then(() => res.sendStatus(204))
   .catch(console.error)
 })
-
-app.get('/api/v1/books/find', (req, res) => {
-  let url = 'https://www.googleapis.com/books/v1/volumes';
-  let query = ''
-  if(req.query.title) query += `+intitle:${req.query.title}`;
-  if(req.query.author) query += `+inauthor:${req.query.author}`;
-  if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
-
-  superagent.get(url)
-    .query({'q': query})
-    .query({'key': GOOGLE_API_KEY})
-    .then(response => response.body.items.map((book, idx) => {
-      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
-      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
-
-      return {
-        title: title ? title : 'No title available',
-        author: authors ? authors[0] : 'No authors available',
-        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
-        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
-        description: description ? description : 'No description available',
-        book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
-      }
-    }))
-    .then(arr => res.send(arr))
-    .catch(console.error)
-})
-
-app.get('/api/v1/books/find/:isbn', (req, res) => {
-  let url = 'https://www.googleapis.com/books/v1/volumes';
-  superagent.get(url)
-    .query({ 'q': `+isbn:${req.params.isbn}`})
-    .query({ 'key': API_KEY })
-    .then(response => response.body.items.map((book, idx) => {
-      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
-      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
-
-      return {
-        title: title ? title : 'No title available',
-        author: authors ? authors[0] : 'No authors available',
-        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
-        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
-        description: description ? description : 'No description available',
-      }
-    }))
-    .then(book => res.send(book[0]))
-    .catch(console.error)
-})
-
 
 app.get('/api/v1/admin', (req, res) => res.send(TOKEN === req.query.token))
 
